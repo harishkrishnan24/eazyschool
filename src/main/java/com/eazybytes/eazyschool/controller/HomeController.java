4144085e4309769912dce7e0a79f5759860a17e7
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-06-16 12:03:55 +0530

    Switch off template caching

--- src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.eazybytes.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value = {"", "/", "home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Harish Krishnan");
         return "home.html";
     }
 }
