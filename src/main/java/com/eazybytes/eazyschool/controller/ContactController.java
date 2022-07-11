14490d24ca5ecaf6a43414dafa2072b9a3c632d1
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-07-11 17:34:29 +0530

    Bean scopes

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -37,6 +37,8 @@ public class ContactController {
             return "contact.html";
         }
         contactService.saveMessageDetails(contact);
+        contactService.setCounter(contactService.getCounter() + 1);
+        log.info("Number of times the Contact form is submitted : " + contactService.getCounter());
         return "redirect:/contact";
     }
 }
--- src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -3,10 +3,18 @@ package com.eazybytes.eazyschool.service;
 import com.eazybytes.eazyschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 @Service
 @Slf4j
+@ApplicationScope
 public class ContactService {
+
+    private int counter = 0;
+
+    public ContactService() {
+        log.info("Contact Service Bean initialized");
+    }
 
     /*
      * Save Contact Details into DB
@@ -19,6 +27,14 @@ public class ContactService {
         log.info(contact.toString());
 
         return isSaved;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
 
